d1b2ee5283bf2711c2bb54f82f65d71fb5e177b5
Roman Yakimkin <r.yakimkin@yandex.ru>
2021-11-11 14:44:54 +0300

    Add Russian language. Add MultilangController and two methods.

---
+++ web/modules/custom/d3/src/Controller/MultilangController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\d3\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Language\LanguageInterface;
+
+/**
+ * Controller routines for multilingual features.
+ */
+class MultilangController extends ControllerBase {
+
+  /**
+   * Get list of all the defined languages in the system.
+   *
+   * @return array
+   *   The render array with languages.
+   */
+  public function getAllLanguages(): array {
+    $languages = [];
+    foreach ($this->languageManager()::getStandardLanguageList() as $item_id => $item) {
+      $languages[] = [
+        'id' => $item_id,
+        'name' => $item[0] . ' ' . $item[1],
+        'direction' => $item[2] ?? 'ltr',
+      ];
+    }
+    return $this->makeLanguagesOutput($languages);
+  }
+
+  /**
+   * Get list of all the active languages of the site.
+   *
+   * @return array
+   *   The render array with languages.
+   */
+  public function getSiteLanguages(): array {
+    $languages = [];
+    foreach ($this->languageManager()->getLanguages() as $lang) {
+      $languages[] = [
+        'id' => $lang->getId(),
+        'name' => $lang->getName(),
+        'direction' => $lang->getDirection(),
+      ];
+    }
+    return $this->makeLanguagesOutput($languages);
+  }
+
+  /**
+   * Make the output table for languages.
+   *
+   * @param array $languages
+   *   The languages list.
+   *
+   * @return array
+   *   The render array with languages.
+   */
+  protected function makeLanguagesOutput(array $languages): array {
+    $header = [
+      $this->t('ID'),
+      $this->t('Name'),
+      $this->t('Direction'),
+    ];
+    $rows = [];
+    foreach ($languages as $language) {
+      $rows[] = [
+        $language['id'],
+        $language['name'],
+        $language['direction'],
+      ];
+    }
+    return [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+  }
+
+}
